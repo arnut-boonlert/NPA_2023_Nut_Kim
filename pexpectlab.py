935a34f29a2582b0f80c0dcc35d9f447be419d0f
arnut-boonlert <arnutboonlert@gmail.com>
2023-02-07 15:59:56 +0700

    add pexpectlab.py

--- pexpectlab.py
+++ pexpectlab.py
@@ -1,1 +1,21 @@
-print("hello world")
+import pexpect
+
+PROMPT = '#'
+IP = '10.0.14.112'
+USERNAME = 'admin'
+PASSWORD = 'cisco'
+COMMAND = 'sh ip int br'
+
+child = pexpect.spawn('telnet ' + IP)
+child.expect('Username')
+child.sendline(USERNAME)
+child.expect('Password')
+child.sendline(PASSWORD)
+child.expect(PROMPT)
+child.sendline(COMMAND)
+child.expect(PROMPT)
+result = child.before
+print(result)
+print()
+print(result.decode('UTF-8'))
+child.sendline('exit')
