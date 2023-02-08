a1e50cc307bbe09aa59d87c845dd6871fb1a54a3
arnut-boonlert <arnutboonlert@gmail.com>
2023-02-08 08:13:06 +0000

    defined function called loopback, created for loop, defined loopback_ip var to be assign, created for loop to send commands to configure ip for loopback interfaces

--- pexpectlab.py
+++ pexpectlab.py
@@ -1,20 +1,29 @@
 import pexpect
 
 PROMPT = '#'
-IP = '172.31.108.4'
 USERNAME = 'admin'
 PASSWORD = 'cisco'
-COMMAND = 'sh ip int br'
 
-devices_ip = ["172.31.108.4", "172.31.108.5", "172.31.108.6"]
-child = pexpect.spawn('telnet ' + IP)
-child.expect('Username')
-child.sendline(USERNAME)
-child.expect('Password')
-child.sendline(PASSWORD)
-child.expect(PROMPT)
-child.sendline(COMMAND)
-child.expect(PROMPT)
-result = child.before
-print(result.decode('UTF-8'))
-child.sendline('exit')
+def loopback():
+    devices_ip = ['172.31.108.4', '172.31.108.5', '172.31.108.6']
+    for ip in devices_ip:
+        child = pexpect.spawn('telnet ' + ip)
+        child.expect('Username')
+        child.sendline(USERNAME)
+        child.expect('Password')
+        child.sendline(PASSWORD)
+        num = str(devices_ip.index(ip)+1)
+        loopback_ip = f"ip add {num}.{num}.{num}.{num} 255.255.255.255"
+        commands = ['conf t' ,'int lo 0', loopback_ip, 'end']
+        for command in commands:
+            child.expect(PROMPT)
+            child.sendline(command)
+        child.expect(PROMPT)
+        child.sendline('show ip int br')
+        child.expect(PROMPT)
+        child.sendline('exit')
+        result = child.before
+        print(result.decode('UTF-8'))
+        print()
+        
+loopback()
