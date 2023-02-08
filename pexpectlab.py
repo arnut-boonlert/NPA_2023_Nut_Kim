78a30522dcd7902a55cb2980cd6c6c9cae154cd5
arnut-boonlert <arnutboonlert@gmail.com>
2023-02-08 10:45:23 +0700

    add for loop and test sh ip int br

--- pexpectlab.py
+++ pexpectlab.py
@@ -6,17 +6,20 @@ USERNAME = 'admin'
 PASSWORD = 'cisco'
 COMMAND = 'sh ip int br'
 
-child = pexpect.spawn('telnet ' + IP)
-child.expect('Username')
-child.sendline(USERNAME)
-child.expect('Password')
-child.sendline(PASSWORD)
-child.expect(PROMPT)
-child.sendline(COMMAND)
-child.expect(PROMPT)
-result = child.before
-print(result)
-print()
-print(result.decode('UTF-8'))
-child.sendline('exit')
-print("test")
+devices_ip = ["172.31.108.4", "172.31.108.5", "172.31.108.6"]
+
+for ip in devices_ip:
+    child = pexpect.spawn('telnet ' + IP)
+    child.expect('Username')
+    child.sendline(USERNAME)
+    child.expect('Password')
+    child.sendline(PASSWORD)
+    child.expect(PROMPT)
+    child.sendline(COMMAND)
+    child.expect(PROMPT)
+    result = child.before
+    print(result)
+    print()
+    print(result.decode('UTF-8'))
+    child.sendline('exit')
+    print("test")
