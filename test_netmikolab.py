f9c9dc5dade0ac6ad818f38ee606d064e04c46c8
arnut-boonlert <arnutboonlert@gmail.com>
2023-02-15 07:50:54 +0000

    starting code for test_netmikolab.py

---
+++ test_netmikolab.py
@@ -0,0 +1,14 @@
+from netmikolab import *
+
+device_ip = '172.31.108.4'
+username = 'admin'
+password = 'cisco'
+device_params = {'device_type': 'cisco_ios', 'ip': device_ip, 'username': username, 'password': password}
+
+
+def test_ip():
+    assert get_ip(device_params, "G0/0") == "172.31.108.4"
+    print("All passed")
+
+if __name__ == '__main__':
+    test_ip()
