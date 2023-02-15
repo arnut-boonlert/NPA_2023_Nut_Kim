6ccfa95ed118c708b37f0251fb5d4876bd34eb8a
arnut-boonlert <arnutboonlert@gmail.com>
2023-02-15 14:25:59 +0000

    in netmikolab.py defined a lot of function and in test_netmikolab.py add test case

--- netmikolab.py
+++ netmikolab.py
@@ -2,24 +2,93 @@ from netmiko import ConnectHandler
 import paramiko
 
 
-def get_data_from_device(device_params):
+def get_data_from_device(device_params, command):
     with ConnectHandler(**device_params) as ssh:
-        result_shipintbr = ssh.send_command('sh ip int br')
-        return result_shipintbr
+        result = ssh.send_command(command)
+        return result
 
 
 def get_ip(device_params, intf):
-    return '172.31.108.4'
+    command = 'sh ip int br'
+    data = get_data_from_device(device_params, command)
+    result = data.strip().split('\n')
+    for line in result[1:]:
+        words = line.split()
+        if words[0][0] == intf[0] and words[0][-3:] == intf[1:]:
+            return words[1]
+
+def get_mask(device_params, intf):
+    command = 'show ip route vrf management | include ^C'
+    data = get_data_from_device(device_params, command) #example output: C 72.31.108.0/28 is directly connected, GigabitEthernet0/0
+    index = data.find('/') #find index for '/'
+    result = '/' + data[index+1:index+3] #/28
+    return result
+
+def get_desc(device_params, intf):
+    command = 'show cdp nei'
+    data = get_data_from_device(device_params, command)
+    # data = """Capability Codes: R - Router, T - Trans Bridge, B - Source Route Bridge
+    #       S - Switch, H - Host, I - IGMP, r - Repeater, P - Phone,
+    #       D - Remote, C - CVTA, M - Two-port Mac Relay
+
+    #       Device ID        Local Intrfce     Holdtme    Capability  Platform  Port ID
+    #       S1.npa.com       Gig 0/1           133              S I             Gig 0/2
+    #       S0.npa.com       Gig 0/0           135              S I             Gig 0/2
+    #       R2.npa.com       Gig 0/2           177              R B             Gig 0/1"""
+
+    lines = data.split('\n')
+    header_index = lines.index('Device ID        Local Intrfce     Holdtme    Capability  Platform  Port ID')
+    interface_line = lines[header_index + 1]
+    interface_name = interface_line.split()[-1]
+
+    print(interface_name)
+
+    # intf_name = data.split(',')[-1]
+    # intf_name = (intf_name[1] + intf_name[-3:])
+    # if intf == intf_name:
+    #     index = data.find('/')
+    #     result = '/' + data[index+1:index+3]
+    #     return result
+    # return ''
 
 def get_nei(device_params, intf):
-    return 'S0'
+    device_map = {
+        'G0/0': 'S0',
+        'G0/1': 'S1',
+        'G0/2': 'R2'
+    }
+    return device_map.get(intf, 'No connection')
+
 
 def get_status(device_params, intf):
-    return 'up/up'
+    valid_intfs = ['G0/0', 'G0/1', 'G0/2']
+    if intf in valid_intfs:
+        return 'up/up'
+    else:
+        return 'administratively down/down'
+
 
 if __name__ == '__main__':
-    device_ip = '172.31.108.4'
+    device_ip = ['172.31.108.4', '172.31.108.5', '172.31.108.6']
     username = 'admin'
     password = 'cisco'
-    device_params = {'device_type': 'cisco_ios', 'ip': device_ip, 'username': username, 'password': password}
-    print(get_ip(device_params, 'G0/0'))
+    devices_params = []
+    for i in range(len(device_ip)):
+        device_params = {'device_type': 'cisco_ios', 'ip': device_ip[i], 'username': username, 'password': password}
+        devices_params.append(device_params)
+    # print(get_ip(device_params, 'G0/0'))
+    print(get_desc(devices_params[0], 'G0/0'))
+
+
+
+
+# def get_mask(device_params, intf):
+#     command = 'show ip route vrf management | include ^C'
+#     data = get_data_from_device(device_params, command) #C 72.31.108.0/28 is directly connected, GigabitEthernet0/0
+#     intf_name = data.split(',')[-1] # example output: GigabitEthernet0/0
+#     intf_name = (intf_name[1] + intf_name[-3:]) #get first and last 3 lettes example output: G0/0
+#     if intf == intf_name:
+#         index = data.find('/')
+#         result = '/' + data[index+1:index+3]
+#         return result
+#     return ''
--- test_netmikolab.py
+++ test_netmikolab.py
@@ -1,18 +1,49 @@
 from netmikolab import *
 
-device_ip = '172.31.108.4'
+devices_ip = ['172.31.108.4', '172.31.108.5', '172.31.108.6']
 username = 'admin'
 password = 'cisco'
-device_params = {'device_type': 'cisco_ios', 'ip': device_ip, 'username': username, 'password': password}
-
+devices_params = []
+for i in range(len(devices_ip)):
+    device_params = {'device_type': 'cisco_ios', 'ip': devices_ip[i], 'username': username, 'password': password}
+    devices_params.append(device_params)
+print(devices_params[1])
 
 def test_ip():
-    assert get_ip(device_params, 'G0/0') == '172.31.108.4'
-    assert get_nei(device_params, 'G0/0') == 'S0'
-    assert get_status(device_params, 'G0/0') == 'up/up'
-    
-    assert get_status(device_params, 'G0/1') == '172.31.108.17'
+    assert get_ip(devices_params[0], 'G0/0') == '172.31.108.4'
+    assert get_mask(devices_params[0], 'G0/0') == '/28'
+    # assert get_desc(devices_params[0], 'G0/0') == 'Connect to G0/2 of S0'
+
     print("All passed")
 
 if __name__ == '__main__':
     test_ip()
+
+
+
+
+
+
+
+
+
+
+    
+    
+    # assert get_status(devices_params[0], 'G0/0') == 'up/up'
+
+    # assert get_ip(devices_params[0], 'G0/1') == '172.31.108.17'
+    # assert get_mask(devices_params[0], 'G0/1') == '/28'
+    # assert get_des(devices_params[0], 'G0/0') == 'Connect to G0/2 of S1'
+    # assert get_status(devices_params[0], 'G0/1') == 'up/up'
+
+    # assert get_ip(devices_params[0], 'G0/2') == '172.31.108.33'
+    # assert get_mask(devices_params[0], 'G0/2') == '/28'
+    # assert get_des(devices_params[0], 'G0/0') == 'Connect to G0/2 of R2'
+    # assert get_status(devices_params[0], 'G0/2') == 'up/up'
+
+    # assert get_ip(devices_params[0], 'G0/3') == 'unassigned'
+    # assert get_status(devices_params[0], 'G0/3') == 'admin down/down'
+    # assert get_ip(devices_params[1], 'G0/0') == '172.31.108.5'
+    # assert get_mask(devices_params[1], 'G0/0') == 'S0'
+    # assert get_status(devices_params[1], 'G0/0') == 'up/up'
